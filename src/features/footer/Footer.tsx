0fbe40d97214cfc9a0c7479bcec1e1c70ab5f312
Peter Trofimov <ptrofimov7@gmail.com>
2021-08-18 21:03:39 +0300

    Added Footer

---
+++ src/features/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+   return (
+      <footer className="footer">
+        <p>© abz.agency specially for the test task</p>
+      </footer>
+   );
+}
+
